91a9705c6abe86f0f0c721a9fa26cdc014db6bcb
gontarenko <gontarenko.dev@gmail.com>
2023-04-16 11:28:37 +0400

    spring-core (lesson #7)

--- spring-app1/src/main/java/org/learning/app/Main.java
+++ spring-app1/src/main/java/org/learning/app/Main.java
@@ -1,21 +1,19 @@
 package org.learning.app;
 
-import org.learning.app.music.Music;
 import org.learning.app.music.MusicPlayer;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        var testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.value());
 
-        Music music = context.getBean("music", Music.class);
-        MusicPlayer player = new MusicPlayer(music);
-        player.playMusic();
+        MusicPlayer classicMusicPlayer = context.getBean("classicMusicPlayer", MusicPlayer.class);
+        System.out.println(classicMusicPlayer.getName() + " with volume = " + classicMusicPlayer.getVolume());
+        classicMusicPlayer.playMusic();
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer rockMusicPlayer = context.getBean("rockMusicPlayer", MusicPlayer.class);
+        System.out.println(rockMusicPlayer.getName() + " with volume = " + rockMusicPlayer.getVolume());
+        rockMusicPlayer.playMusic();
 
         context.close();
     }
--- spring-app1/src/main/java/org/learning/app/music/MusicPlayer.java
+++ spring-app1/src/main/java/org/learning/app/music/MusicPlayer.java
@@ -2,9 +2,27 @@ package org.learning.app.music;
 
 public class MusicPlayer {
     private final Music music;
+    private String name;
+    private int volume;
 
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
